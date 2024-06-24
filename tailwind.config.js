b9ebf4eb13c426d4eb8928f2611e2779546f54d3
Hmazah <alhamza2012@gmail.com>
2024-06-24 10:00:00 +0300

    add public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+  fontFamily: {
+    zentry: ['Zentry', 'sans-serif'],
+    general: ['General', 'sans-serif'],
+    'circular-web': ['Circular-web', 'sans-serif'],
+    'robert-medium': ['Robert-medium', 'sans-serif'],
+    'robert-regular': ['Robert-regular', 'sans-serif'],
+  
+  
+  
+  },
   },
   plugins: [],
 }
