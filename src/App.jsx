52a7fb3224044407bba12c6dba89771411149bd7
Hmazah <alhamza2012@gmail.com>
2024-06-26 10:00:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <h1 className='text-4xl text-red-500 text-center font-bold  underline'>Hello World</h1>
+    </main>
+  )
+}
+
+export default App
