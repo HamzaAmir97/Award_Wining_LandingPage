62e7ed780d8ff4fc024ca4603c918565e25a9c8c
Hmazah <alhamza2012@gmail.com>
2024-06-27 10:00:00 +0300

    coustmaize tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <main>
-      <h1 className='text-4xl text-red-500 text-center font-bold  underline'>Hello World</h1>
+      <h1 className='text-4xl text-violet-300 text-center font-bold  underline'>Hello World</h1>
     </main>
   )
 }
