ddd0325ebd0874763dce056654429bd34fe53849
Hmazah <alhamza2012@gmail.com>
2024-06-30 10:00:00 +0300

    edite hero sec

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Hero } from './componemts/Hero'
 function App() {
   return (
-    <main className='  relative min-h-screen w-screen overflow-x-hidden '>
+    <main className='relative min-h-screen w-screen overflow-x-hidden '>
       <Hero />
     </main>
   )
