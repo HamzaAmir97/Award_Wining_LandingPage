3db0a39ac6078013c705e7b52b04792c70aab406
Hmazah <alhamza2012@gmail.com>
2024-07-14 10:00:00 +0300

    add  icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Hero from "./componemts/Hero";
 import About from "./componemts/About";
 import Navbar from "./componemts/Navbar";
+import Features from "./componemts/Featuers";
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
      <About />
-      {/* <Features />
-      <Story />
+      <Features />
+      {/* <Story />
       <Contact />
       <Footer /> */}
     </main>
---
+++ src/componemts/Featuers.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Featuers() {
+  return (
+    <section className='bg-black pb-52'>
+        <div className='container mx-auto'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+                
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Featuers
